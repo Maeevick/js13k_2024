6650d35ad0dc76a978058f423af93848ad140098
Maeevick <maeevick@gmail.com>
2024-08-17 23:01:30 +0200

    chore: setup single file plugin (update vite config)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   server: { port: 5000, strictPort: true, open: true },
@@ -6,5 +7,5 @@ export default defineConfig({
     minify: "esbuild", // recheck with terser when the game will grow. For the moment esbuild wins.
     cssMinify: "esbuild", // recheck with lightningcss when the game will grow. For the moment esbuild wins.
   },
-  plugins: [],
+  plugins: [viteSingleFile()],
 });
